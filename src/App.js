caddb116cd9d154ae29b554ebc43db3b7c3dce70
Weronika Kansy <weronika.kansy@gmail.com>
2022-05-12 00:15:40 +0200

    UI created, styles added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
 function App() {
-  return <div className="container"></div>;
+  return (
+    <div className="container">
+      <h1>How fast do you type?</h1>
+      <textarea name="text" />
+      <h4>Remaining time: </h4>
+      <button>Start</button>
+      <h1>Word count: </h1>
+    </div>
+  );
 }
 
 export default App;
