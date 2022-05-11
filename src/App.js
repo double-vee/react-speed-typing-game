b0067e0730dd632eccdf356d3c29ec326b3fe66a
Weronika Kansy <weronika.kansy@gmail.com>
2022-05-12 00:57:43 +0200

    Using the useState hook and the onChange event, saved the text from textarea in state on every keystroke

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
+import { useState } from "react";
+
 function App() {
+  const [text, setText] = useState("");
+
+  const handleChange = (e) => {
+    setText(e.target.value);
+  };
+
   return (
     <div className="container">
       <h1>How fast do you type?</h1>
-      <textarea name="text" />
+      <textarea name="text" value={text} onChange={handleChange} />
       <h4>Remaining time: </h4>
       <button>Start</button>
       <h1>Word count: </h1>
