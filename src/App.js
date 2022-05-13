2313b384a77a87661ffb98b4012f43dc86e07e2e
Weronika Kansy <weronika.kansy@gmail.com>
2022-05-13 20:07:41 +0200

    Inside useEffect, set the countdown of the remaining time using setTimeout and a new state; time displayed in the UI

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
   const [text, setText] = useState("");
   const [wordCount, setWordCount] = useState(0);
+  const [timeRemaining, setTimeRemaining] = useState(5);
 
   const handleChange = (e) => {
     setText(e.target.value);
@@ -17,11 +18,19 @@ function App() {
     return wordsArray.length;
   };
 
+  useEffect(() => {
+    if (timeRemaining > 0) {
+      setTimeout(() => {
+        setTimeRemaining(timeRemaining - 1);
+      }, 1000);
+    }
+  }, [timeRemaining]);
+
   return (
     <div className="container">
       <h1>How fast do you type?</h1>
       <textarea name="text" value={text} onChange={handleChange} />
-      <h4>Remaining time: </h4>
+      <h4>Remaining time: {timeRemaining}</h4>
       <button>Start</button>
       <h1>Word count: </h1>
     </div>
